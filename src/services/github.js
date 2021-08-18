97b58c799f83fbdee1ba6e6afe86ceb42e824385
culi <culitif@tuta.io>
2021-08-18 01:40:28 -0700

    write fetch to get user and repos

---
+++ src/services/github.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+const API = 'https://api.github.com/users'
+
+const fetchUser = async username => {
+  return await fetch(`${API}/${username}`)
+    .then(resp => resp.json())
+    .catch(err => console.error(err))
+  ;
+};
+
+const fetchUserRepos = async username => {
+  return await fetch(`${API}/${username}/repos`)
+    .then(resp => resp.json())
+    .catch(err => console.error(err))
+  ;
+};
+
+export { fetchUser, fetchUserRepos };
