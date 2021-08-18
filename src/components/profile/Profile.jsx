f194cfed33748271acaa65936d5b5815bf7ba988
culi <culitif@tuta.io>
2021-08-18 14:24:46 -0700

    profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Profile.scss';
 
-const Profile = props => {
-  console.log(props);
+const Profile = ({ profile }) => {
   
   return <>
     <div className="Profile">
-      
+      <h3><a href={profile.url}>{profile.login}</a></h3>
+      <div>
+        <span><span>{profile.public_repos}</span> public repos</span>
+        <span>
+          user since <span>{new Date(profile.created_at).toDateString()}</span>
+        </span>
+      </div>
+      <span>{profile.bio}</span>
     </div>
   </>;
+};
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired
 };
 
 export default Profile;
