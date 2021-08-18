a9c4b5839a77849a7bdc849abf5c7373b7c95a28
culi <culitif@tuta.io>
2021-08-18 14:24:32 -0700

    RepoList component

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
+import './RepoList.scss';
 
-const RepoList = props => {
-  console.log(props);
+const RepoList = ({ repos }) => {
   
   return <>
-    <div className="RepoList">
-      
-    </div>
+    <ul className="RepoList">
+      {repos.map(repo => <RepoItem key={repo.id} repo={repo} />)}
+    </ul>
   </>;
+};
+
+RepoList.propTypes = {
+  repos: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default RepoList;
